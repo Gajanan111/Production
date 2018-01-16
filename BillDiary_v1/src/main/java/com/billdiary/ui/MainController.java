080758e305ecd0167d07067d573f9168312dcb77
Gajanan <gajanangaikwad999@gmail.com>
2018-01-16 17:27:16 +0530

    Gajanan : Added a progress bar

--- BillDiary_v1/src/main/java/com/billdiary/ui/MainController.java
+++ BillDiary_v1/src/main/java/com/billdiary/ui/MainController.java
@@ -12,19 +12,73 @@ import com.billdiary.utility.Constants;
 import com.billdiary.utility.DAOConstants;
 import com.billdiary.utility.URLS;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class MainController extends Application{
 	
+	
 	final static Logger LOGGER = Logger.getLogger(MainController.class);
-	private static final SpringFxmlLoader loader =SpringFxmlLoader.getInstance();
+	private static SpringFxmlLoader loader;
 	
-	
+	private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+    private Stage mainStage;
+    private static final int SPLASH_WIDTH = 676;
+    private static final int SPLASH_HEIGHT = 227;
+    
+   /* public static final String SPLASH_IMAGE =
+    		"http://fxexperience.com/wp-content/uploads/2010/06/logo.png";
+    */
+    public static final String SPLASH_IMAGE ="";
+    
+    @Override
+    public void init() {
+        ImageView splash = new ImageView(new Image(
+                getClass().getResourceAsStream("/icon/BillDiary.png")
+        ));
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH - 20);
+        progressText = new Label("Will find friends for peanuts . . .");
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash, loadProgress, progressText);
+        progressText.setAlignment(Pos.CENTER);
+        splashLayout.setStyle(
+                "-fx-padding: 5; " +
+                "-fx-background-color: cornsilk; " +
+                "-fx-border-width:5; " +
+                "-fx-border-color: " +
+                    "linear-gradient(" +
+                        "to bottom, " +
+                        "chocolate, " +
+                        "derive(chocolate, 50%)" +
+                    ");"
+        );
+        splashLayout.setEffect(new DropShadow());
+    }
+    
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -45,19 +99,86 @@ public class MainController extends Application{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		final Task<ObservableList<String>> splashfriendTask = new Task<ObservableList<String>>() {
+            @Override
+            protected ObservableList<String> call() throws InterruptedException {
+                ObservableList<String> foundFriends =
+                        FXCollections.<String>observableArrayList();
+                /*ObservableList<String> availableFriends =
+                        FXCollections.observableArrayList(
+                                "Fili", "Kili", "Oin", "Gloin", "Thorin",
+                                "Dwalin", "Balin", "Bifur", "Bofur",
+                                "Bombur", "Dori", "Nori", "Ori"
+                        );*/
+                updateMessage("Initializing.");
+                for(int i = 0; i < 4; i++) {
+                	Thread.sleep(4);
+                	updateProgress(i + 1, 10);
+                	foundFriends.add("A");
+                	 loader=SpringFxmlLoader.getInstance();
+                	updateMessage("Initializing..");
+                }
+               
+                updateMessage("Initializing...");
+                Thread.sleep(4);
+                updateMessage("All friends found.");
+                return foundFriends;
+            }
+         };
+		
+         showSplashScreen(stage,splashfriendTask,() -> showMainStage(splashfriendTask.valueProperty()));
+         new Thread(splashfriendTask).start();
+		//showMainStage();
+	}
+	
+	private void showSplashScreen(final Stage initStage,Task<?> task,InitCompletionHandler initCompletionHandler) {
+		progressText.textProperty().bind(task.messageProperty());
+        loadProgress.progressProperty().bind(task.progressProperty());
+        task.stateProperty().addListener((observableValue, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED) {
+                loadProgress.progressProperty().unbind();
+                loadProgress.setProgress(1);
+                initStage.toFront();
+                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+                fadeSplash.setFromValue(1.0);
+                fadeSplash.setToValue(0.0);
+                fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+                fadeSplash.play();
+
+                initCompletionHandler.complete();
+            } // todo add code to gracefully handle other task states.
+        });
+
+        Scene splashScene = new Scene(splashLayout, Color.TRANSPARENT);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.initStyle(StageStyle.TRANSPARENT);
+        initStage.setAlwaysOnTop(true);
+        initStage.show();
+		
+		
+		
+	}
+	public interface InitCompletionHandler {
+        void complete();
+}
+	
+	
+	private void showMainStage(ReadOnlyObjectProperty<ObservableList<String>> friends) {
 		Parent root = (Parent) loader.load(URLS.LOGIN_PAGE);
 		//Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Login.fxml"));
 		//Stage stage = new Stage();
 		
-		
-		
+		mainStage = new Stage(StageStyle.DECORATED);
 		Scene scene = new Scene(root,ScreenController.getScreenWidth(), ScreenController.getScreenHeight()); 
-        stage.setTitle(Constants.APPLICATION_TITLE);
-        stage.setScene(scene);
-        stage.show();	
-       
+		mainStage.setTitle(Constants.APPLICATION_TITLE);
+		mainStage.setScene(scene);
+		mainStage.show();	
 	}
-	
 	
 	
 	
